e71a9ca6867614a77938ade9981996d1f6b6bdea
Durand <vlad.zdk@gmail.com>
2014-06-18 13:23:57 +0200

    modif pizza

--- modif_pizza.php
+++ modif_pizza.php
@@ -20,7 +20,7 @@
 
 	$num=($_POST["num"]);
 	
-	$reponse = mysqli_query($db, "SELECT * FROM Pizza WHERE PIZ_IdPizza=$num");
+	$reponse = mysqli_query($db, "SELECT * FROM Pizza WHERE PIZ_IdPizza='$num'");
 	
 	$donnees = mysqli_fetch_assoc($reponse)
 		?>
@@ -28,12 +28,14 @@
 			<form method="post" action="postModif_pizza.php">
 			<fieldset>
 			<p>
+			<?php echo $donnees['PIZ_IdPizza']; ?>
 			<label for="name">Nom : </label><input required name="name" type="text" id="name" value="<?php echo $donnees['PIZ_Nom']; ?>"/><br />
-			<label for="price">Prix : </label><input required type="text" name="price" id="price"  value="<?php echo $donnees['PIZ_Prix']; ?>/>
+			<label for="price">Prix : </label><input required type="text" name="price" id="price"  value="<?php echo $donnees['PIZ_Prix']; ?>"/><br />
+			<input type="hidden" name="num" value="<?php echo $num; ?>">
 			</p>
 			</fieldset>
 			<table>
-			<tr><td><input type="submit" value="Ajouter" /></td>
+			<tr><td><input type="submit" value="Modifier" /></td>
 			<td><input type="reset" value="Reset"></td></tr></table></form>
 	
 						
--- postModif_pizza.php
+++ postModif_pizza.php
@@ -10,36 +10,35 @@
 	</head>
 
 	<body>
-		<h1>Pizza</h1>
-		<a href="ajout_pizza.php">Ajouter une pizza</a>
-		<br/><br/>
-		<div id ="list">
+		<h1> <a href="home.php">Pizza</a></h1>
+		
 		<?php
 
 	if($db = MySQLi_connect("localhost","projetAgile",'pizza', 'Agile', 0, '/media/sds1/home/alx22/private/mysql/socket'))
 		echo '';
 	else
 		echo 'Erreur';
-	
-	$reponse = mysqli_query($db, "SELECT * FROM Pizza");
-	
-	while ($donnees = mysqli_fetch_assoc($reponse))
-	{
-		if($donnees['PIZ_Valide']==1){
-		?>	>Nom pizza : <?php echo $donnees['PIZ_Nom']; ?>
-			<br/>
-			Prix pizza : <?php echo $donnees['PIZ_Prix']; ?> &euro;
-			<br/>
-			<form method="post" action="modif_pizza.php">
-			<input type="hidden" name="num" value="<?php echo $donnees['PIZ_IdPizza']; ?>">
-			<input type="submit" value="Modifier" />
-			</form>
+
+	$name=strtoupper(addslashes($_POST["name"]));
+	$price=($_POST["price"]);
+	$num=($_POST["num"]);
+
+	$query = "UPDATE Pizza SET PIZ_Nom = '$name', PIZ_Prix = '$price' WHERE PIZ_IdPizza = '$num';";
+
+  	$sql = mysqli_query($db, $query);
+  	$errr=mysqli_error($db);
+
+  	if($sql){
+	        echo '<div id="good">     
+	            <label>Pizza modifiée avec succès</label>
+	            </div>';
+		?>									
 			<br/><br/>
 				        	<?php
-		}
+	
 	}
 	mysqli_free_result($reponse);
-	?>				</div>			
+	?>							
 			</body>
 	<footer>
 		<div>
