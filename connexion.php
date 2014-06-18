6f99d0e7dbee232c3e0bb790cf285d17f58ff791
jcadel <jordancadel@MacBook-Pro-de-Jordan.local>
2014-06-18 11:03:08 +0200

    Ajout connexion home

---
+++ connexion.php
@@ -0,0 +1,20 @@
+<html>
+	<body>
+		<?php
+			echo '<form method="post" action="main.php">
+			<fieldset>
+			<legend>Connexion</legend>
+			<p>
+			<label for="pseudo">Pseudo : </label><input name="pseudo" type="text" id="pseudo" /><br />
+			<label for="password">Mot de Passe : </label><input type="password" name="password" id="password" />
+			</p>
+			</fieldset>
+			<table>
+			<tr><td><input type="submit" value="Connexion" /></td>
+			<td><input type="reset" value="Reset"></td></tr></table></form>
+			</div>
+			</body>
+			</html>';
+		?>
+	</body>
+</html>
