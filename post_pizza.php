2f1ea95ae1eea642aee6ab493f58ea2d393e1adf
Durand <vlad.zdk@gmail.com>
2014-06-18 11:33:23 +0200

    postPizza

---
+++ post_pizza.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Gestion de pizza</title>
+		<!-- IUT Caen - DUT Info (2013-2015) -->
+	</head>
+
+	<body>
+		<h1>Pizza</h1>
+		
+		<?php
+
+	if($db = MySQLi_connect("localhost","projetAgile",'pizza', 'Agile', 0, '/media/sds1/home/alx22/private/mysql/socket'))
+		echo '';
+	else
+		echo 'Erreur';
+
+	$name=strtoupper(addslashes($_POST["name"]));
+	$price=($_POST["price"]);
+
+	$query = "INSERT INTO Pizza (PIZ_Nom, PIZ_Prix) VALUES ('$name', '$prix')";
+
+  	$sql = mysqli_query($db, $query);
+  	$errr=mysqli_error($db);
+
+  	if($sql){
+	        echo '<div id="good">     
+	            <label>Pizza ajouté avec succès</label>
+	            </div>';
+		?>									
+			<br/><br/>
+				        	<?php
+	
+	}
+	mysqli_free_result($reponse);
+	?>							
+			</body>
+	<footer>
+		<div>
+			&copy; Projet Agile <?php echo date('Y');?>
+		</div>
+	</footer>
+
+</html>
