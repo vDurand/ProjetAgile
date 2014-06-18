4a0654128daaae509d67992fa9e58393fa296a87
jcadel <jordancadel@MacBook-Pro-de-Jordan.local>
2014-06-18 11:12:28 +0200

    Changement titre

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Intranet association Revivre</title>
+		<title>Gestion de pizza</title>
 		<!-- IUT Caen - DUT Info (2013-2015) -->
 	</head>
 
