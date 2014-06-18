557d9a688d3285388c1df454b5135f78556f51fc
jcadel <jordancadel@MacBook-Pro-de-Jordan.local>
2014-06-18 11:47:49 +0200

    style

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Gestion de pizza</title>
+		<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 		<!-- IUT Caen - DUT Info (2013-2015) -->
 	</head>
 
