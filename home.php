4307960248d4025400046d16157e1c39ab248b71
Durand <vlad.zdk@gmail.com>
2014-06-18 11:08:26 +0200

    connexion

--- home.php
+++ home.php
@@ -1,8 +1,38 @@
 <!DOCTYPE html>
 
 <html>
+
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Intranet association Revivre</title>
+		<!-- IUT Caen - DUT Info (2013-2015) -->
 	</head>
+
 	<body>
-	</body>
+		
+		<?php
+
+	if($db = MySQLi_connect("localhost","projetAgile",'pizza', 'Agile', 0, '/media/sds1/home/alx22/private/mysql/socket'))
+		echo '';
+	else
+		echo 'Erreur';
+	$i = 2;
+	$reponse = mysqli_query($db, "SELECT * FROM Pizza");
+	
+	while ($donnees = mysqli_fetch_assoc($reponse))
+	{
+		?>									<?php echo $donnees['PIZ_Nom']; ?>
+				        	<?php
+	
+	}
+	mysqli_free_result($reponse);
+	?>							
+			</body>
+	
+	<footer>
+		<div>
+			Projet Agile <?php echo date('Y');?>
+		</div>
+	</footer>
+
 </html>
