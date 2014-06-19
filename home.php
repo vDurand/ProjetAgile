2e34fcfb8b9bbcf47a5946e0f5004946c6a6b474
jcadel <jordancadel@MacBook-Pro-de-Jordan.local>
2014-06-19 09:57:41 +0200

    test

--- home.php
+++ home.php
@@ -2,7 +2,13 @@
     include('bandeau.php');
 ?>
 <div id="corps">
-	Welcome
+	<div id = "info">
+		<h3>Horaires</h3></br>
+		<pre>Ouvert 7j/7
+			11h - 13h30 / 18h - 22h30
+		</pre>
+			
+	</div>
 </div>
 <?php  
   include('footer.php');
