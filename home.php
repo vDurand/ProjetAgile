fdab92aa94fb62265053f97262f704e7e6c6ad44
jcadel <jordancadel@MacBook-Pro-de-Jordan.local>
2014-06-19 10:57:03 +0200

    motif accueil

--- home.php
+++ home.php
@@ -3,11 +3,28 @@
 ?>
 <div id="corps">
 	<div id = "info">
-		<h3>Horaires</h3></br>
-		<pre>Ouvert 7j/7
-			11h - 13h30 / 18h - 22h30
+		<img alt="banniere" src="banniere.jpg" width="90%"/> 
+		<div id="horaires">
+		<h4>Horaires</h4>
+		<pre>
+Ouvert 7j/7
+11h - 13h30 / 18h - 22h30
 		</pre>
-			
+		</div>
+		<div id="adresse" >
+		<h4>Adresse</h4>
+		<pre>
+Rue Anton Tchekhov - Campus III
+14123 Ifs
+		</pre>
+		</div>
+		<div id="contact">
+		<h4>Contact</h4>
+		<pre>
+Tél : 02.31.525.500
+Fax : 02.31.525.502
+		</pre>
+		</div>
 	</div>
 </div>
 <?php  
