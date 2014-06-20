eca6c2a092854a458a51e5ce4401d2e5ea7f0109
rahmanovic <barahmane90@gmail.com>
2014-06-20 14:56:40 +0200

    Affichage date CA

--- CA.php
+++ CA.php
@@ -10,9 +10,10 @@ if($db = MySQLi_connect("localhost","projetAgile",'pizza', 'Agile', 0, '/media/s
 			echo 'Erreur';
 		$sql="SELECT date(ORD_Fin) FROM `Order` WHERE ORD_Fin != '' order by date(ORD_Fin) asc;";
 		$req = mysqli_query($db, $sql); //or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-		$data = mysqli_fetch_assoc($req);
-		if($data['date(ORD_Fin)'] != "0000-00-00")
-			echo $data['date(ORD_Fin)']."<br>";
+		while($data = mysqli_fetch_assoc($req)){
+			if($data['date(ORD_Fin)'] != "0000-00-00")
+				echo $data['date(ORD_Fin)']."<br>";
+		}
 ?>
 </div>
 <?php  
